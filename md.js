9ca0a2f553b12a0a0b0b7bed930b7de956f30573
Unknown <bdistin@gmail.com>
2018-02-08 14:43:06 -0600

    initial shareable eslint

---
+++ md.js
@@ -0,0 +1,17 @@
+module.exports = {
+	extends: './.eslintrc.json',
+	env: {
+		node: true,
+		es6: true
+	},
+	plugins: [
+		'markdown'
+	],
+	rules: {
+		'no-unused-vars': 0,
+		'no-undef': 0,
+		'no-invalid-this': 0,
+		'no-empty-function': 0,
+		'no-unused-expressions': 0
+	}
+};
